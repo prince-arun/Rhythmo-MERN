e91b14b9c97418ddb704cd15ee9bb1523c22677e
prince-arun <prince.skillsafari@gmail.com>
2023-09-14 18:06:10 +0530

    Production build fixed

--- server/server.js
+++ server/server.js
@@ -10,9 +10,28 @@ const userRoute = require("./routes/userRoute");
 const songsRoute = require("./routes/songsRoute");
 const adminRoute = require("./routes/adminRoute");
 
+const PORT = process.env.PORT || 5000;
+//----------------------------------------------------------------------------------------------------
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URL);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+//---------------------------------------------------------------------------------------------------
+
 app.use("/api/users", userRoute);
 app.use("/api/songs", songsRoute);
 app.use("/api/admin", adminRoute);
-const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`server started at port  ${PORT}`));
+//---------------------------------------------------------------------------------------------------------
+//Connect to the database before listening
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("listening for requests");
+  });
+});
+//--------------------------------------------------------------------------------------------------
